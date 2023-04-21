a6162b8f778c20b3a45cf947857710c0f73c961c
Pourbaix <mi.pourbaix@students.ephec.be>
2023-04-21 09:13:09 +0200

    [ADD] Completing demo

--- django_components_app/urls.py
+++ django_components_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name="components"
+app_name="django_components_app"
 urlpatterns = [
     path('', views.MainPage.as_view(), name="main_page"),
 ]
